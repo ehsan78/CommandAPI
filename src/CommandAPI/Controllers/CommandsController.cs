604cc28c3c68f7ab53c5c3b3e0755154481abb49
ehsan <ehsan_ahmed78@hotmail.com>
2020-11-26 15:15:24 +0400

    Added Command controller  to API Project

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,15 +1,25 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+
 namespace CommandAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
+          private readonly ICommandAPIRepo _repository;
+    public CommandsController(ICommandAPIRepo repository)
+    {
+        _repository=repository;
+    }
+      
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+       public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
-            return new string[] {"this", "is", "hard", "coded"};
+           var commandItems = _repository.GetAllCommands();
+           return Ok(commandItems);
         }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
 
 namespace CommandAPI
 {
@@ -18,7 +19,7 @@ namespace CommandAPI
         {
 
             services.AddControllers();
-
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
